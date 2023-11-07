669d73352c11b141e04ae9a7c88ab506cc99d21d
Ember Chow <emberchow.businiess@iu.edu>
2023-11-07 14:55:16 -0800

    Trash unused files

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 from setuptools import setup
 
+from setuptools import find_packages, setup
+
 if __name__ == '__main__':
-    setup()
+    setup(
+        name='forcedimension_core',
+        packages=find_packages(),
+        version='1.0.0',
+        python_requires=">=3.6, <3.7",
+        install_requires=[
+            "setuptools>=42",
+            "wheel",
+        ],
+        requires=[
+            "typing_extensions >= 4.1.1, <5",
+            "pydantic>=1.9.2, <2",
+            "numpy>=1.16.4, <2",
+        ]
+    )
